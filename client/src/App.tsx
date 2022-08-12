7b724c88b3379607bd1902b72f9f5874ec818489
Andreas Alexander <ndre19223@gmail.com>
2022-08-13 00:07:49 +0700

    setup login and register routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,25 @@ import {
   Stack,
 } from '@chakra-ui/react';
 import Navbar from './components/navbar';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './pages/login';
+import Register from './pages/register';
+import Home from './pages/home';
+import ErrorPage from './pages/error-page';
 
 function App() {
   return (
-    <Stack>
-      <Navbar />
-    </Stack>
+    <Router>
+      <Stack>
+        <Navbar />
+      </Stack>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/error-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function errorPage() {
+  return (
+    <div>errorPage</div>
+  )
+}
+
+export default errorPage;
