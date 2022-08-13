b7e1fd96f535064cc3228182f59baffe99c5c927
Andreas Alexander <ndre19223@gmail.com>
2022-08-13 23:09:22 +0700

    link register page with jwt

--- client/src/pages/users/login.tsx
+++ client/src/pages/users/login.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Center, FormControl, FormLabel, Heading, Input, Stack } from '@chakra-ui/react';
+import Axios from 'axios';
 
-function login() {
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginStatus, setLoginStatus] = useState('false');
+
+  Axios.defaults.withCredentials = true;
+
+  const login = () => {
+    Axios.post("http://localhost:8000/auth/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (!response) {
+        setLoginStatus('false');
+        console.log(loginStatus);
+      }
+      else {
+        setLoginStatus('true');
+        console.log(loginStatus);
+      }
+    });
+  }
+
   return (
     <Stack>
       <Center mt="20">
@@ -9,10 +32,21 @@ function login() {
       </Center>
       <FormControl isRequired ps="56" pe="56" pt="8">
         <FormLabel>Username</FormLabel>
-        <Input placeholder='Username' />
+        <Input
+          placeholder='Username'
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
         <FormLabel pt="4">Password</FormLabel>
-        <Input type='password' placeholder='Password' />
-        <Button colorScheme='teal' mt="4">
+        <Input
+          type='password'
+          placeholder='Password'
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <Button colorScheme='teal' mt="4" onClick={login}>
           Login
         </Button>
       </FormControl>
@@ -20,4 +54,4 @@ function login() {
   );
 }
 
-export default login;
+export default Login;
