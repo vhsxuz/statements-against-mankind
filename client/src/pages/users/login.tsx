49f5cada4fe078ff2640c3b203baea9cdbf6c766
Andreas Alexander <ndre19223@gmail.com>
2022-08-14 00:06:03 +0700

    create login and logout

--- client/src/pages/users/login.tsx
+++ client/src/pages/users/login.tsx
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
 import { Button, Center, FormControl, FormLabel, Heading, Input, Stack } from '@chakra-ui/react';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loginStatus, setLoginStatus] = useState(false);
 
-  Axios.defaults.withCredentials = true;
+  Axios.defaults.withCredentials = false;
 
   const login = () => {
     Axios.post('http://localhost:8000/api/v1/auth/login', {
       username: username,
       password: password,
     }).then((response) => {
-      let token = response.data.access;
-      localStorage.setItem("SavedToken", 'Bearer ' + token);
-      Axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+      let token = response.data.userToken;
+      localStorage.setItem("SavedToken", token);
+      Axios.defaults.headers.common['Authorization'] = token;
       setLoginStatus(true)
     }).catch((error) => {
       console.log(error)
@@ -27,7 +28,6 @@ function Login() {
     <Stack>
       <Center mt="20">
         <Heading size='lg'>Login</Heading>
-        <Heading> {loginStatus === false ? "false" : "true"}</Heading>
       </Center>
       <FormControl isRequired ps="56" pe="56" pt="8">
         <FormLabel>Username</FormLabel>
@@ -45,9 +45,11 @@ function Login() {
             setPassword(e.target.value);
           }}
         />
-        <Button colorScheme='teal' mt="4" onClick={login}>
-          Login
-        </Button>
+        <Link to={loginStatus === true ? "/" : "/login"}>
+          <Button colorScheme='teal' mt="4" onClick={login}>
+            Login
+          </Button>
+        </Link>
       </FormControl>
 
     </Stack>
