a1e1aa5f476118ab7bf49a489992b8bb2a7610f0
Andreas Alexander <ndre19223@gmail.com>
2022-08-13 08:46:14 +0700

    create login and register page

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Button, Center, FormControl, FormLabel, Heading, Input, Stack } from '@chakra-ui/react';
 
 function register() {
   return (
-    <div>register</div>
+    <Stack>
+      <Center mt="20">
+        <Heading size='lg'>Sign Up</Heading>
+      </Center>
+      <FormControl isRequired ps="56" pe="56" pt="8">
+        <FormLabel>Username</FormLabel>
+        <Input placeholder='Username' />
+        <FormLabel pt="4">Password</FormLabel>
+        <Input type='password' placeholder='Password' />
+        <Button colorScheme='teal' mt="4">
+          Sign Up
+        </Button>
+      </FormControl>
+    </Stack>
   )
 }
 
